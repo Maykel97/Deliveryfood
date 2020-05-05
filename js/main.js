7349a5bfab68cf8eb76da9d543b8b95a072daa50
Maykel97 <63522318+Maykel97@users.noreply.github.com>
2020-05-05 12:52:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,92 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+// день первый
+// открытие окна авторизации
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+
+let login = localStorage.getItem("user");
+
+function toogleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+// когда пользователь авторизован
+
+function authorized() {
+  // выход
+  function logOut() {
+    login = null;
+    // обнуление localStorage
+    localStorage.removeItem("user");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
+
+    checkAucth();
+  }
+
+  console.log("Авторизован");
+
+  // имя пользователя рядом с кнопкой выход
+  userName.textContent = login;
+
+  // когда пользователь авторизован кнопка войти убирается
+  buttonAuth.style.display = "none";
+
+  // кнопка выход
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+  buttonOut.addEventListener("click", logOut);
+}
+
+// // когда пользователь не авторизован
+
+function notAuthorized() {
+  console.log("Не авторизован");
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+
+    // чтоб не приходилось заного логинится когда обновляем страницу
+    localStorage.setItem("user", login);
+
+    toogleModalAuth();
+    buttonAuth.removeEventListener("click", toogleModalAuth);
+    closeAuth.removeEventListener("click", toogleModalAuth);
+    logInForm.removeEventListener("submit", logIn);
+    // очищение поля логин
+    logInForm.reset();
+    checkAucth();
+  }
+
+  buttonAuth.addEventListener("click", toogleModalAuth);
+  closeAuth.addEventListener("click", toogleModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAucth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAucth();
